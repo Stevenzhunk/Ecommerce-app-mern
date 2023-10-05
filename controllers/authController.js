bed35e7f483c61e84ca19dab7db5889f82592822
Stevenzhunk <kikexgaz@gmail.com>
2023-10-05 17:35:23 -0300

    authorization added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,8 @@
-import { hashPassword } from '../helpers/hashPasword.js';
+import { hashPassword, comparePassword } from '../helpers/hashPasword.js';
 import userModel from '../models/userModel.js';
+import jwt from 'jsonwebtoken';
 
+//Register
 export const registerController = async (req, res) => {
   try {
     const { name, email, password, phone, address } = req.body;
@@ -42,4 +44,60 @@ export const registerController = async (req, res) => {
       error,
     });
   }
+};
+
+//Login
+export const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    //validation
+    if (!email || !password) {
+      return res.status(400).send({
+        success: false,
+        message: 'Invalid email or password',
+      });
+    }
+    //check user
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: 'Email not found',
+      });
+    }
+    const match = await comparePassword(password, user.password);
+    if (!match) {
+      return res.status(401).send({
+        success: false,
+        message: 'Password mismatch',
+      });
+    }
+    //token
+
+    const token = await jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: 60 * 60,
+    });
+    res.status(200).send({
+      success: true,
+      message: 'Token was successfully',
+      user: {
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+        address: user.address,
+      },
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      mesage: 'Error Login',
+      error,
+    });
+  }
+};
+
+export const testController = async (req, res) => {
+  res.send('Protected Route');
 };
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,11 +1,20 @@
 import express from 'express';
-import { registerController } from '../controllers/authController.js';
-
+import {
+  loginController,
+  registerController,
+  testController,
+} from '../controllers/authController.js';
+import { requireSigIn } from '../middlewares/AuthMiddleware.js';
 //router Obj
 const router = express.Router();
 
 //Routing
 //Register || Method post
 router.post('/register', registerController);
+
+//Login || Post
+router.post('/login', loginController);
+
+router.get('/test', requireSigIn, testController);
 
 export default router;
