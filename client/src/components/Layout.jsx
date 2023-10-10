775643bd3440d5880e80f60c4e83270fbcc2efc9
Stevenzhunk <kikexgaz@gmail.com>
2023-10-09 22:46:53 -0300

    structure basic and route register/login

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -7,19 +7,24 @@ const Layout = ({ children, description, keywords, author, title }) => {
   return (
     <>
       <Helmet>
-        <Helmet>
-          <meta charSet="utf-8" />
-          <meta name="description" content={description} />
-          <meta name="keywords" content={keywords} />
-          <meta name="author" content={author} />
-          <title>{title}</title>
-        </Helmet>
+        <meta charSet="utf-8" />
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+        <meta name="author" content={author} />
+        <title>{title}</title>
       </Helmet>
       <Header />
       <main style={{ minHeight: '70vh' }}>{children}</main>
       <Footer />
     </>
   );
+};
+
+Layout.defaultProps = {
+  title: 'Ecommerce app - shop now',
+  description: 'mern stack project',
+  keywords: 'mern,react,node,mongodb, express',
+  author: 'StevenzHunk - Luis Becerra',
 };
 
 export default Layout;
